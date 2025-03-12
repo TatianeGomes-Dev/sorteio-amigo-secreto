86ed261c4d390719882305d0f8b51ca55f161659
Tatiane Gomese <tatianegomes.dev@gmail.com>
2025-03-12 10:23:02 -0300

    Projeto Amigo

---
+++ app.js
@@ -0,0 +1,24 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionar() {
+  adicionarAmigo() =  document.getElementById('amigo');
+}
+
+    
+
+    
+// function limparCampo() {     // Limpar o campo chute.
+//     chute = document.querySelector('input');
+//     chute.value = '';
+     
+
+
+
+// function reiniciarJogo() {
+//     numeroSecreto = gerarNumeroAleatorio();
+//     limparCampo();
+//     tentativas = 1;
+//     exibirMensagemInicial();
+//     document.getElementById('reiniciar').setAttribute('disabled', true)
